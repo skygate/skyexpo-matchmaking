230d5b892a007ba44fe1ea27908631b866dc39b7
Pawel-Mlynarczyk <pawel.mlynarczyk@skygate.io>
2019-12-02 11:15:28 +0100

    [SKYEX-13] Created core for routing, fixed docker-compose for web-app

--- web-app/src/App.js
+++ web-app/src/App.js
@@ -1,13 +1,29 @@
 import React from 'react';
+import { Redirect, Router, Route, Switch } from 'react-router-dom';
 
 import './App.css';
 import 'antd/dist/antd.css';
 
 import { RegisterCompanyForm } from './companyRegistrationForm/container/RegisterCompanyForm';
+import { WelcomePage } from './welcomePage/WelcomePage';
+import { history } from './history';
 
 const App = () => (
     <div className="App">
-        <RegisterCompanyForm />
+        <Router history={history}>
+            <Switch>
+                <Route exact path="/">
+                    <WelcomePage />
+                </Route>
+                <Route exact path="/startup">
+                    <RegisterCompanyForm />
+                </Route>
+                <Route exact path="/company">
+                    <h1>Company</h1>
+                </Route>
+                <Redirect to="/" />
+            </Switch>
+        </Router>
     </div>
 );
 
