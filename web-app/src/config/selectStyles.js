31a40ede0e58ab81d2b85f69549bf06881256f3b
Arkadiusz Sapek <sapek.arkadiusz@gmail.com>
2020-03-12 13:24:06 +0100

    [SKYEX-43] Replace antd Select by react-select

---
+++ web-app/src/config/selectStyles.js
@@ -0,0 +1,41 @@
+import { color, fontSize } from './values';
+
+export const selectStyles = {
+    option: provided => ({
+        ...provided,
+        color: color.primaryFont,
+        backgroundColor: color.elementBackground,
+        padding: '0.55rem 0 0.55rem 2rem',
+    }),
+    valueContainer: provided => ({
+        ...provided,
+        color: color.primaryFont,
+        backgroundColor: color.elementBackground,
+        opacity: 0.7,
+        borderRadius: '0.3rem 0 0 0.3rem',
+        padding: '0.55rem 0 0.55rem 2rem',
+    }),
+    indicatorsContainer: provided => ({
+        ...provided,
+        opacity: 0.7,
+        borderRadius: '0 0.3rem 0.3rem 0',
+        backgroundColor: color.elementBackground,
+    }),
+    control: provided => ({
+        ...provided,
+        border: 'none',
+        boxShadow: 'none',
+        '&:hover': {
+            border: 'none',
+        },
+        fontSize: fontSize.normal,
+    }),
+    container: provided => ({
+        ...provided,
+        marginTop: '0.5rem',
+    }),
+    indicatorSeparator: provided => ({
+        ...provided,
+        display: 'none',
+    }),
+};
