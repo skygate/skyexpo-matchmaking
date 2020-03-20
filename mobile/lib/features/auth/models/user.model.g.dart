0dafe52d851dcf761447f5e985bacf53dec3ccd0
Arkadiusz Sapek <sapek.arkadiusz@gmail.com>
2020-03-20 15:07:49 +0100

    [SKYEX-62] Add missing json helper classes

---
+++ mobile/lib/features/auth/models/user.model.g.dart
@@ -0,0 +1,23 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'user.model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+User _$UserFromJson(Map<String, dynamic> json) {
+  return User(
+    id: json['id'] as String,
+    email: json['email'] as String,
+    name: json['name'] as String,
+    token: json['token'] as String,
+  );
+}
+
+Map<String, dynamic> _$UserToJson(User instance) => <String, dynamic>{
+      'id': instance.id,
+      'email': instance.email,
+      'name': instance.name,
+      'token': instance.token,
+    };
