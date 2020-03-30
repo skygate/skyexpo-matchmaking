557e16b7aae1b5ff0204b839db51e03789ab613a
Arkadiusz Sapek <sapek.arkadiusz@gmail.com>
2020-03-30 16:25:37 +0200

    [SKYEX-52] Add rounded corners to bottom nav

--- mobile/lib/core/widgets/bottom_navigation_widget.dart
+++ mobile/lib/core/widgets/bottom_navigation_widget.dart
@@ -1,30 +1,45 @@
 import 'package:flutter/material.dart';
 
 import 'package:mobile/config/colors_config.dart' show AppColor;
-import 'package:mobile/core/configs/bottom_navigation_elements_config.dart';
+import '../configs/bottom_navigation_elements_config.dart'
+    show bottomNavigationElementsConfig;
+
+final radius = Radius.circular(30);
+final bordeRadius = BorderRadius.only(topRight: radius, topLeft: radius);
 
 class BottomNavigation extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return BottomNavigationBar(
-        backgroundColor: AppColor.elementsBackground.value,
-        currentIndex: 1,
-        iconSize: 30,
-        unselectedIconTheme: IconThemeData(color: Colors.grey),
-        showUnselectedLabels: true,
-        items: bottomNavigationElementsConfig
-            .map(
-              (element) => BottomNavigationBarItem(
-                icon: Padding(
-                  padding: EdgeInsets.only(top: 20, bottom: 0),
-                  child: Icon(
-                    element['icon'],
-                    color: element['color'],
-                  ),
-                ),
-                title: Text('.'),
-              ),
-            )
-            .toList());
+    return Container(
+        decoration: BoxDecoration(
+          borderRadius: bordeRadius,
+          boxShadow: [
+            BoxShadow(color: Colors.grey[400], spreadRadius: 0, blurRadius: 1),
+          ],
+        ),
+        child: ClipRRect(
+            borderRadius: bordeRadius,
+            child: Container(
+                color: Colors.white,
+                child: BottomNavigationBar(
+                    backgroundColor: AppColor.elementsBackground.value,
+                    currentIndex: 1,
+                    iconSize: 30,
+                    unselectedIconTheme: IconThemeData(color: Colors.grey),
+                    showUnselectedLabels: true,
+                    items: bottomNavigationElementsConfig
+                        .map(
+                          (element) => BottomNavigationBarItem(
+                            icon: Padding(
+                              padding: EdgeInsets.only(top: 20),
+                              child: Icon(
+                                element['icon'],
+                                color: element['color'],
+                              ),
+                            ),
+                            title: Text(''),
+                          ),
+                        )
+                        .toList()))));
   }
 }
